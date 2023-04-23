bdbddbe72427470ba77207c57672b79afd73bece
ryuzaki <ryuzaki>
2023-04-23 20:03:27 +0530

    api gateway

---
+++ src/main/kotlin/com/inblack67/micro/spring/ecom/apigateway/ApigatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.inblack67.micro.spring.ecom.apigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApigatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApigatewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/inblack67/micro/spring/ecom/apigateway/ApigatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.inblack67.micro.spring.ecom.apigateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
